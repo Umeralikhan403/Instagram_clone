772f057508c5a2a7a6fe56aa812e0dd4237b453b
Umer Ali <92618390+Umeralikhan403@users.noreply.github.com>
2023-04-02 14:34:12 +0500

    provider

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,12 +2,23 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:instagram_clone/resources/storage_methods.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot snap =
+        await _firestore.collection('users').doc(currentUser.uid).get();
+
+    return model.User.fromSnap(snap);
+  }
+
   // signup method
   Future<String> signUpUser({
     required String email,
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_providers.dart';
+import 'package:provider/provider.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is Mobile')),
+    model.User user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
+      body: Center(child: Text(user.username)),
     );
   }
 }
