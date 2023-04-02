c094091557d4f456e30a7826b3157dd44837e6d0
Umer Ali <92618390+Umeralikhan403@users.noreply.github.com>
2023-04-02 15:02:08 +0500

    hh

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,7 +2,6 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:instagram_clone/resources/storage_methods.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
