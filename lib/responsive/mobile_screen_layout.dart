b130c98b8c30135f6f654b915b78ecbde0cb4700
Umer Ali <92618390+Umeralikhan403@users.noreply.github.com>
2023-04-05 14:58:19 +0500

    tab bar

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/providers/user_providers.dart';
+import 'package:instagram_clone/utils/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram_clone/models/user.dart' as model;
 
@@ -11,11 +12,88 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     model.User user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(child: Text(user.username)),
+      body: PageView(
+        children: const [
+          Text('Home'),
+          Text('Search'),
+          Text('Favorite'),
+          Text('Add'),
+          Text('User'),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              semanticLabel: '',
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: '',
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle,
+              semanticLabel: '',
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              semanticLabel: '',
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              semanticLabel: '',
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+          ),
+        ],
+        onTap: navigationTapped,
+      ),
     );
   }
 }
