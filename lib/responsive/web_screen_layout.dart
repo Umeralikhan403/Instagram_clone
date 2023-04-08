d37ce904cebaac521134fdca8935daa1669ebdc9
Umer Ali <92618390+Umeralikhan403@users.noreply.github.com>
2023-04-08 10:59:25 +0500

    web screen

--- lib/responsive/web_screen_layout.dart
+++ lib/responsive/web_screen_layout.dart
@@ -6,7 +6,7 @@ class WebScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text('This is Web')),
+      body: Center(child: Text('This is Web Screen')),
     );
   }
 }
